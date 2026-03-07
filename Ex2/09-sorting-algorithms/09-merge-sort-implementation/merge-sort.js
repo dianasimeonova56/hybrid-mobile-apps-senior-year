06deaa30dcc6e3b1ed9317316fbc4f60b10bc30d
dianasim <diana.simeonova56@gmail.com>
2026-03-07 21:37:51 +0200

    09 folder done

--- Ex2/09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
+++ Ex2/09-sorting-algorithms/09-merge-sort-implementation/merge-sort.js
@@ -1,3 +1,34 @@
-function mergeSort() {}
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+}
+
+function merge(left, right) {
+    const arr = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            arr.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            arr.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return [
+        ...arr, 
+        ...left.slice(leftIndex), 
+        ...right.slice(rightIndex)
+    ];
+}
 
 module.exports = mergeSort;
